f0f116e3b68817ce646e97b6c14061965f4f24ab
Vamshi Dega Reddy <55816325+vamshidega@users.noreply.github.com>
2019-10-01 00:12:15 -0400

    Update GPIO_Legacy_Driver.c

--- GPIO_driver/GPIO_Legacy_Driver.c
+++ GPIO_driver/GPIO_Legacy_Driver.c
@@ -38,8 +38,6 @@
 struct cdev *gpio_cdev;
 dev_t new_gpio_dev;
 static struct class *gpio_class;
-struct device dev;
-
 
 static int gpio_dev_open(struct inode *inode,
 			    struct file  *file)
@@ -54,6 +52,7 @@ static int gpio_dev_release(struct inode *inode,
 	return SUCCESS;
 }
 
+/*Read call will read the GPIO pin and send it to the User space*/
 static ssize_t gpio_dev_read(struct file *file,
 		            char *buf,
 			    size_t lbuf,
@@ -62,19 +61,21 @@ static ssize_t gpio_dev_read(struct file *file,
 	int gpio_value;
 	int gpio_data=0;
 	int ret;
-
+	// Read the specific GPIO pin
 	gpio_value = gpio_get_value(GPIO_PIN_NUM_18);
 	if(gpio_value == 1) gpio_data|=0x01;
-
+	
+        //Read the specific GPIO pin
 	gpio_value = gpio_get_value(GPIO_PIN_NUM_19);
-	if(gpio_value == 1) gpio_data|=0x01;
-
+	if(gpio_value == 1) gpio_data|=0x02;
+        
+	//Send the value to the User space
 	ret = copy_to_user(buf,(char*) &gpio_data,sizeof(gpio_data));
 	return sizeof(int);
 
 }
 
-
+/*Write call will write the data recieved from the User to the GPIO line*/
 static ssize_t gpio_dev_write(struct file *file,
 		            const char *buf,
 			    size_t lbuf,
@@ -88,11 +89,13 @@ static ssize_t gpio_dev_write(struct file *file,
 	 		buf,  
 	        	lbuf ); 
 
-	// Sent value should be decoded for 3 LEDs. 
+	// Sent value should be decoded for 2 LEDs. 
 	value = (value_to_write & 0x01)? 0:1;
+	//Write the value to the GPIO line
 	gpio_set_value(GPIO_PIN_NUM_18, value);
 
 	value = (value_to_write & 0x02)? 0:1;
+	//Write the value to the GPIO line
 	gpio_set_value(GPIO_PIN_NUM_19, value);
 	
 	return sizeof(int);
@@ -106,20 +109,28 @@ static struct file_operations gpio_dev_fops = {
 	.release = gpio_dev_release,
 };
 
+
+/* Module  initialization
+* 1. Considered a Character device which will be allocated dynamically
+* 2. Create the /dev entry and /sys entry for the device
+* 3. Request the GPIO pins and set the direction of the GPIO pin
+* 4. Export the GPIO pin to the sysfs
+*/
 static __init int gpio_dev_init(void)
 {
 	int ret;
-
+	//Dynamically allocate the Major and Minor numbers for the device
 	if (alloc_chrdev_region (&new_gpio_dev, 0, DEV_COUNT, DEV_NAME) < 0) {
             printk (KERN_ERR "failed to reserve major/minor range\n");
             return -1;
         }
-
+	//Allocate the cdev instance
         if (!(gpio_cdev = cdev_alloc ())) {
             printk (KERN_ERR "cdev_alloc() failed\n");
             unregister_chrdev_region (new_gpio_dev, DEV_COUNT);
             return -1;
  	}
+	//attach the cdev with the vfs with the file operations structure
 	cdev_init(gpio_cdev,&gpio_dev_fops);
 
 	ret=cdev_add(gpio_cdev,new_gpio_dev,DEV_COUNT);
@@ -144,17 +155,17 @@ static __init int gpio_dev_init(void)
 	if(ret<0) goto Error_Handle;
 
 	//Export the GPIO pins to the sysfs
-     ret = gpio_export(GPIO_PIN_NUM_18, true);
-     if(ret<0) printk(KERN_INFO "Error exporting the gpio 18\n");
+        ret = gpio_export(GPIO_PIN_NUM_18, true);
+        if(ret<0) printk(KERN_INFO "Error exporting the gpio 18\n");
 
-     ret = gpio_export(GPIO_PIN_NUM_19, true);
-     if(ret<0) printk(KERN_INFO "Error exporting the gpio 19\n");
-
-
-    gpio_class = class_create(THIS_MODULE, "gpio_rasp_cls");
+        ret = gpio_export(GPIO_PIN_NUM_19, true);
+        if(ret<0) printk(KERN_INFO "Error exporting the gpio 19\n");
+         
+	// create the /dev entry for the device
+         gpio_class = class_create(THIS_MODULE, "gpio_rasp_cls");
 	 device_create(gpio_class, NULL, new_gpio_dev, NULL,"%s","gpio_dev");
 
-   return 0;
+         return 0;
 
 Error_Handle:
 	printk(KERN_INFO "failed to request the GPIO pins\n");
@@ -163,15 +174,21 @@ Error_Handle:
 	return ret;
 		
 }
-
+/* Module Exit
+* Clean up all the allocations has been done in the init.
+*/
 static __exit void  gpio_dev_exit(void)
 {
+	// Free the GPIO pins that are requested
 	 gpio_free(GPIO_PIN_NUM_18);
 	 gpio_free(GPIO_PIN_NUM_19);
+	//Unexport the GPIO pins from the sysfs
 	 gpio_unexport(GPIO_PIN_NUM_18);
 	 gpio_unexport(GPIO_PIN_NUM_19);
-     device_destroy(gpio_class, new_gpio_dev);
+	//Delete the devfs and sysfs entry of the device
+         device_destroy(gpio_class, new_gpio_dev);
 	 class_destroy(gpio_class);
+	//delete the cdev instance and remove the major and Minor numbers that are allocated
 	 cdev_del(gpio_cdev);
 	 unregister_chrdev_region(new_gpio_dev,DEV_COUNT);
 	 printk(KERN_INFO "\n Driver unregistered \n");
